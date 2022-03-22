e405d96e9b47fd90f7ad11d9ee9b4579c144e7ff
Talisson Sozinho <talissonlutero8080@gmail.com>
2022-03-21 22:21:02 -0300

    feito - requisito 10 e 11

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const colorPalleteContainer = document.getElementById('color-palette');
 const pixelBoardContainer = document.getElementById('pixel-board');
 const clearBoardButton = document.getElementById('clear-board');
+const generateBoardButton = document.getElementById('generate-board');
+const boardScaleSize = document.getElementById('board-size');
 
 const COLOR_SELECTED = 'color selected';
 
 const boardConfig = {
   width: 200,
   height: 200,
-  arrayEdgePixel: [],
-  scale: 3,
+  scale: 5,
 };
 
 function removeSelectedColor() {
@@ -40,34 +41,13 @@ function dye(event) {
 
 pixelBoardContainer.addEventListener('click', dye);
 
-clearBoardButton.addEventListener('click', () => {
+function clearBoard() {
   for (let index = 0; index < pixelBoardContainer.children.length; index += 1) {
     pixelBoardContainer.children[index].style.backgroundColor = '#FFFFFF';
   }
-});
-
-function calcMMC(number1, number2) {
-  const higherNumber = number1 > number2 ? number1 : number2;
-  for (let index = higherNumber; index > 0; index -= 1) {
-    if (number1 % index === 0 && number2 % index === 0) {
-      return index;
-    }
-  }
 }
 
-function arrayOfPossibleScale() {
-  const maxDivider = calcMMC(boardConfig.width, boardConfig.height);
-  const array = [];
-  for (let index = 2; index <= maxDivider; index += 1) {
-    if (boardConfig.width % index === 0 && boardConfig.height % index === 0) {
-      array.push(index);
-    }
-  }
-  array.reverse();
-  return array;
-}
-
-boardConfig.arrayEdgePixel = arrayOfPossibleScale();
+clearBoardButton.addEventListener('click', clearBoard);
 
 function inicializaColorPalette() {
   const backgroundsColors = ['#000000', '#FF0000', '#00FF00', '#0000FF'];
@@ -83,16 +63,37 @@ function inicializaColorPalette() {
 inicializaColorPalette();
 
 function inicializaPixelBoard() {
-  const quantityOfPixel = (boardConfig.width * boardConfig.height)
-                          / boardConfig.arrayEdgePixel[boardConfig.scale] ** 2;
+  const quantityOfPixel = boardConfig.scale ** 2;
+  pixelBoardContainer.style.width = `${40 * boardConfig.scale}px`;
+  pixelBoardContainer.style.height = `${40 * boardConfig.scale}px`;
+
+  pixelBoardContainer.innerHTML = null;
+
   for (let index = 0; index < quantityOfPixel; index += 1) {
     const pixel = document.createElement('div');
     pixel.className = 'pixel';
     pixel.style.backgroundColor = '#FFFFFF';
     pixel.style.border = '1px solid #000000';
-    pixel.style.height = `${boardConfig.arrayEdgePixel[boardConfig.scale]}px`;
-    pixel.style.width = `${boardConfig.arrayEdgePixel[boardConfig.scale]}px`;
+    pixel.style.height = '40px';
+    pixel.style.width = '40px';
     pixelBoardContainer.appendChild(pixel);
   }
 }
 inicializaPixelBoard();
+
+function generateNewBoard() {
+  if (!boardScaleSize.value) {
+    alert('Board inválido!');
+    return;
+  }
+  boardConfig.scale = boardScaleSize.value;
+  if (boardScaleSize.value > 50) {
+    boardConfig.scale = 50;
+  }
+  if (boardScaleSize.value < 5) {
+    boardConfig.scale = 5;
+  }
+  inicializaPixelBoard();
+}
+
+generateBoardButton.addEventListener('click', generateNewBoard);
